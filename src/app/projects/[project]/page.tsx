0620f31f361535b7e8842c8cbbd959310489550d
Asaiah Henson <Hensonasaiah21@gmail.com>
2024-08-06 00:41:01 -0700

    intial commit

--- src/app/projects/[project]/page.tsx
+++ src/app/projects/[project]/page.tsx
@@ -17,22 +17,45 @@ const Project = async ({params}:Props) => {
       <h2 className="text-2xl md:text-3xl font-bold py-5">{project.name}</h2>
       <div className="flex justify-between gap-8 flex-wrap md:flex-nowrap">
         <div className="relative w-full md:w-[50%] h-[400px]">
-          <Image src={project.image} className='object-cover' fill alt={project.name} />
+          <Image
+            src={project.image}
+            className="object-cover"
+            fill
+            alt={project.name}
+          />
         </div>
         <div className="md:w-[50%] text-justify mb-4">
           <PortableText value={project.content} />
-          <span className='font-medium text-gray-500 mt-4'><strong className='text-black'>Author:</strong> Asaiah Henson</span>
-          <div className='flex gap-4 items-center mt-5 bg-slate-200 w-fit p-2 rounded-full'>
-            <Link href={""} className='bg-slate-300 p-1.5 hover:bg-slate-400 rounded-full'>
+          <span className="font-medium text-gray-500 mt-4">
+            <strong className="text-black">Author:</strong> Asaiah Henson
+          </span>
+          <div className="flex gap-4 items-center mt-5 bg-slate-200 w-fit p-2 rounded-full">
+            <Link
+              href={"https://web.facebook.com/asaiah.henson.7"}
+              target="_blank"
+              className="bg-slate-300 p-1.5 hover:bg-slate-400 rounded-full"
+            >
               <Facebook />
             </Link>
-            <Link href={""} className='bg-slate-300 p-1.5 hover:bg-slate-400 rounded-full'>
+            <Link
+              href={"https://www.linkedin.com/in/asaiah-henson-440bb62a6/"}
+              target='_blank'
+              className="bg-slate-300 p-1.5 hover:bg-slate-400 rounded-full"
+            >
               <Linkedin />
             </Link>
-            <Link href={""} className='bg-slate-300 p-1.5 hover:bg-slate-400 rounded-full'>
+            <Link
+              href={"https://www.instagram.com/hensonasaiah/"}
+              target='_blank'
+              className="bg-slate-300 p-1.5 hover:bg-slate-400 rounded-full"
+            >
               <Instagram />
             </Link>
-            <Link href={""} className='bg-slate-300 p-1.5 hover:bg-slate-400 rounded-full'>
+            <Link
+              href={"https://x.com/dreamer_webdev"}
+              target="_blank"
+              className="bg-slate-300 p-1.5 hover:bg-slate-400 rounded-full"
+            >
               <Twitter />
             </Link>
           </div>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,29 +4,35 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='w-[95%] md:w-[90%] lg:w-[80%] mx-auto p-3 flex items-center justify-between bg-slate-100 rounded-full'>
-      <div className='bg-gradient-to-r from-red-600 via-blue-600 bg-clip-text text-transparent font-bold  md:text-2xl to-purple-400'>DreamerBlog</div>
+    <div className="w-[95%] md:w-[90%] lg:w-[80%] mx-auto p-3 flex items-center justify-between bg-slate-100 rounded-full">
+      <div className="bg-gradient-to-r from-red-600 via-blue-600 bg-clip-text text-transparent font-bold  md:text-2xl to-purple-400">
+        DreamerBlog
+      </div>
       <div className="flex gap-4 items-center bg-slate-200 w-fit p-2 rounded-full">
         <Link
-          href={""}
+          href={"https://web.facebook.com/asaiah.henson.7"}
+          target="_blank"
           className="bg-slate-300 p-1 md:p-1.5 hover:bg-slate-400 rounded-full"
         >
           <Facebook />
         </Link>
         <Link
-          href={""}
+          href={"https://www.linkedin.com/in/asaiah-henson-440bb62a6/"}
+          target="_blank"
           className="bg-slate-300 p-1 md:p-1.5 hover:bg-slate-400 rounded-full"
         >
           <Linkedin />
         </Link>
         <Link
-          href={""}
+          href={"https://www.instagram.com/hensonasaiah/"}
+          target="_blank"
           className="bg-slate-300 p-1 md:p-1.5 hover:bg-slate-400 rounded-full"
         >
           <Instagram />
         </Link>
         <Link
-          href={""}
+          href={"https://x.com/dreamer_webdev"}
+          target="_blank"
           className="bg-slate-300 p-1 md:p-1.5 hover:bg-slate-400 rounded-full"
         >
           <Twitter />
